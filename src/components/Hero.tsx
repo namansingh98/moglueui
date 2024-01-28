a8a929d07bd29c7d54609b87b202bfd633a9f89e
namansingh98 <naman.rathore@outlook.com>
2024-01-28 23:02:06 +0530

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="grid grid-cols-2 gap-2 items-center">
+        <div className="flex flex-col items-center justify-center p-5">
+          <div className=" border-dashed border-2 dark:border-white  border-black p-5">
+            <h1 className="text-6xl font-bold mb-5">
+              Let me introduce the new app that connects strangers via video /
+              chat
+            </h1>
+          </div>
+          <div className="flex gap-2 items-center justify-center p-5">
+            <div className="flex gap-2 items-center justify-center">
+            <Button variant={"outline"}>Let's Connect</Button>
+-/
+            <Button variant={"link"}>Know more</Button>
+            </div>
+          </div>
+        </div>
+        <div>right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
