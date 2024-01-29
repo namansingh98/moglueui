e0a8d33ba228be2897c549a6998aa30735ca49c2
namansingh98 <naman.rathore@outlook.com>
2024-01-29 15:32:49 +0530

    some changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,35 @@
+"use client";
 import React from "react";
 import { Button } from "./ui/button";
-import { TextGen } from "./HomeText";
+
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+import { BackgroundBeams } from "./ui/Background-beams";
+
+const words = `Let me introduce the new app that connects strangers via video /
+chat`;
 
 const Hero = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col items-center justify-center p-5">
-        <div className=" border-dashed border-2 dark:border-white  border-black p-5">
-          <h1 className="text-9xl font-bold mb-5">
-            <TextGen />
-          </h1>
-        </div>
-        <div className="flex gap-2 items-center justify-center p-5">
-          <div className="flex gap-2 items-center justify-center">
-            <Button variant={"outline"}>Let&apos;s Connect</Button>
-            -/
-            <Button variant={"link"}>Know more</Button>
+    <div className="h-screen w-full rounded-md dark:bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-4xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold"></h1>
+        <p></p>
+      
+          <div className="flex flex-col items-center justify-center p-5">
+            <div className=" border-dashed border-2 dark:border-white  border-black p-5">
+              <TextGenerateEffect words={words} className="text-5xl dark:text-white " />
+            </div>
+            <div className="flex gap-2 items-center justify-center p-5">
+              <div className="flex gap-2 items-center justify-center">
+                <Button variant={"outline"}>Let&apos;s Connect</Button>
+                -/
+                <Button variant={"link"}>Know more</Button>
+              </div>
+            </div>
           </div>
-        </div>
+      
       </div>
+      <BackgroundBeams />
     </div>
   );
 };
--- src/components/HomeText.tsx
+++ src/components/HomeText.tsx
@@ -1,10 +1,17 @@
-"use client";
-import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+// "use client";
+// import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
-const words = ` Let me introduce the new app that connects strangers via video /
-chat
-`;
+// const words = `Let me introduce the new app that connects strangers via video /
+// chat`;
 
-export function TextGen() {
-  return <TextGenerateEffect words={words} />;
-}
+// export function TextGen() {
+//   return (
+//   <div >
+  
+//   <TextGenerateEffect words={words} />;
+  
+//   </div>
+
+
+//   )
+// }
