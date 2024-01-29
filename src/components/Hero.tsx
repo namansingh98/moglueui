106b4fcc4cd09fff23d8902413b27081cf470fe1
namansingh98 <naman.rathore@outlook.com>
2024-01-29 18:50:34 +0530

    home changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,13 +4,15 @@ import { Button } from "./ui/button";
 
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 import { BackgroundBeams } from "./ui/Background-beams";
+import { SparklesPreview } from "./Sparkles-Main";
 
 const words = `Let me introduce the new app that connects strangers via video /
 chat`;
 
 const Hero = () => {
   return (
-    <div className="h-screen w-full rounded-md dark:bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+<>
+<div className="h-screen w-full rounded-md dark:bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-4xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold"></h1>
         <p></p>
@@ -31,6 +33,12 @@ const Hero = () => {
       </div>
       <BackgroundBeams />
     </div>
+
+{/* <SparklesPreview /> */}  
+{/* additional hero main code if current is not suitable  */}
+
+
+</>
   );
 };
 
