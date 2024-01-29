b114ab7c90af5b85199bb2da42061d300c206c1b
namansingh98 <naman.rathore@outlook.com>
2024-01-29 14:40:26 +0530

    some main changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,29 +1,22 @@
 import React from "react";
 import { Button } from "./ui/button";
-import Image from "next/image";
-import temp from "../../public/temp.jpeg"
+import { TextGen } from "./HomeText";
 
 const Hero = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="grid grid-cols-2 gap-2 items-center">
-        <div className="flex flex-col items-center justify-center p-5">
-          <div className=" border-dashed border-2 dark:border-white  border-black p-5">
-            <h1 className="text-6xl font-bold mb-5">
-              Let me introduce the new app that connects strangers via video /
-              chat
-            </h1>
-          </div>
-          <div className="flex gap-2 items-center justify-center p-5">
-            <div className="flex gap-2 items-center justify-center">
-            <Button variant={"outline"}>Let's Connect</Button>
--/
-            <Button variant={"link"}>Know more</Button>
-            </div>
-          </div>
+      <div className="flex flex-col items-center justify-center p-5">
+        <div className=" border-dashed border-2 dark:border-white  border-black p-5">
+          <h1 className="text-9xl font-bold mb-5">
+            <TextGen />
+          </h1>
         </div>
-        <div className="flex justify-center items-center">
-          <Image src={temp} alt="img"/>
+        <div className="flex gap-2 items-center justify-center p-5">
+          <div className="flex gap-2 items-center justify-center">
+            <Button variant={"outline"}>Let&apos;s Connect</Button>
+            -/
+            <Button variant={"link"}>Know more</Button>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/HomeText.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+
+const words = ` Let me introduce the new app that connects strangers via video /
+chat
+`;
+
+export function TextGen() {
+  return <TextGenerateEffect words={words} />;
+}
