9efe0fffd2af0d25e8d8ff00aeb067ec47fc9f22
namansingh98 <naman.rathore@outlook.com>
2024-01-30 23:09:04 +0530

    Hero page update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,6 +29,9 @@ const Hero = () => {
             <div className="flex gap-2 items-center justify-center p-5">
               <div className="flex gap-2 items-center justify-center">
                 <Button variant={"outline"}>Let&apos;s Connect</Button>
+                <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        Shimmer
+      </button>
                 -/
                 <Button variant={"link"}>Know more</Button>
               </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,6 +76,7 @@ const config: Config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      
     },
   },
   plugins: [require("tailwindcss-animate"), addVariablesForColors],
