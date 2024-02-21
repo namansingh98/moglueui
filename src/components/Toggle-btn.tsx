f311205746e1cae4ab69af1b13d846181fa992ea
Naman Rathore <108014558+namansingh98@users.noreply.github.com>
2024-02-21 10:41:49 +0530

    Update Toggle-btn.tsx

--- src/components/Toggle-btn.tsx
+++ src/components/Toggle-btn.tsx
@@ -1,62 +1,24 @@
-"use client";
-
-import * as React from "react";
-import { useTheme } from "next-themes";
-import { IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
-
-// import { Button } from "@/components/ui/button"
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu"
-
-import { Switch } from "@/components/ui/switch";
 export function Togglebtn() {
-  const { setTheme } = useTheme();
-const switchHandler =()=>{
-  !setTheme("dark")} ? ("light") : ("system")
-}
+  const { theme, setTheme } = useTheme();
+
+  const switchHandler = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    // <DropdownMenu>
-    //   <DropdownMenuTrigger asChild>
-    //     <Button variant="ghost" size="icon">
-    //       <IoSunnyOutline className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-    //       <IoMoonOutline className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    //       <span className="sr-only">Toggle theme</span>
-    //     </Button>
-    //   </DropdownMenuTrigger>
-    //   <DropdownMenuContent align="end">
-    //     <DropdownMenuItem onClick={() => setTheme("light")}>
-    //       Light
-    //     </DropdownMenuItem>
-    //     <DropdownMenuItem onClick={() => setTheme("dark")}>
-    //       Dark
-    //     </DropdownMenuItem>
-    //     <DropdownMenuItem onClick={() => setTheme("system")}>
-    //       System
-    //     </DropdownMenuItem>
-    //   </DropdownMenuContent>
-    // </DropdownMenu>
     <div className="flex items-center space-x-2">
       <label>
         <IoMoonOutline />
       </label>
-      <switch
+      <Switch
         id="dark"
-        onClick={() => switchHandler()
+        onClick={switchHandler}
         className="w-10 h-6 rounded-full bg-gray-300"
       />
-
       <label>
         <IoSunnyOutline />
       </label>
     </div>
   );
 }
-
-// export function SwitchDemo() {
-//   return (
-//   )
-// }
