695a72f43ea9a77cc37559fb151a425ad9ac4061
namansingh98 <naman.rathore@outlook.com>
2024-03-07 21:57:50 +0530

    chat component ready

--- src/components/VidoeChat.tsx
+++ src/components/VidoeChat.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const VidoeChat = () => {
   return (
-    <div className='bg-blue-900'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.</div>
+    <div className='bg-blue-900 flex items-center jus'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ut optio nam dolor, eum suscipit quis sint facere dicta autem cupiditate! Libero suscipit tempore ad nisi itaque delectus culpa optio.
+      </div>
   )
 }
 
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import VidoeChat from "@/components/VidoeChat";
 import Conversation from "@/components/Conversation";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 const Chat: React.FC = () => {
   const [conversationVisible, setConversationVisible] = useState(false);
 
@@ -11,15 +11,17 @@ const Chat: React.FC = () => {
 
   return (
     <div className="flex  flex-col w-full h-full items-center justify-center">
-      <div className="relative top-20  flex justify-center items-center p-5">
+      <div className="relative  top-8  flex justify-center items-center p-5">
         <VidoeChat />
         <div>
-        <Button variant={"outline"}  onClick={toggleConversation} style={{ display: conversationVisible ? 'none' : 'block' }}>
-          Show Conversation
-        </Button>
-        {conversationVisible && (
-          <Conversation onClose={toggleConversation} />
-        )}
+          <Button
+            variant={"outline"}
+            onClick={toggleConversation}
+            style={{ display: conversationVisible ? "none" : "block" }}
+          >
+            Show Conversation
+          </Button>
+          {conversationVisible && <Conversation onClose={toggleConversation} />}
         </div>
       </div>
     </div>
